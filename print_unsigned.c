d41ccf62b16d84816fccfbd2cf265f84d5a7e729
Abdallah Mufleh <amufleh@c2r4s1.42amman.com>
2025-09-02 09:19:16 +0300

    fix unsigned int

---
+++ print_unsigned.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_unsigned.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amufleh <amufleh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/02 09:18:43 by amufleh           #+#    #+#             */
+/*   Updated: 2025/09/02 09:18:50 by amufleh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	put_unsint(unsigned int num)
+{
+	char	c;
+
+	if (num >= 10)
+	{
+		put_unsint(num / 10);
+	}
+	c = (num % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	print_unsigned(unsigned int num)
+{
+	int	count;
+
+	count = unsigned_len(num);
+	put_unsint (num);
+	return(count);
+}
